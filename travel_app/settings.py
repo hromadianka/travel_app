9844cbda704ed4f52a36ccd7a77276d9876f95dd
hromadianka <99715792+hromadianka@users.noreply.github.com>
2026-06-10 19:51:54 +0300

    Add pagination

--- travel_app/settings.py
+++ travel_app/settings.py
@@ -136,4 +136,6 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10
 }
