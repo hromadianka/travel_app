58f63a2154bd1f72adbc7972d1bc2b6f52294c28
hromadianka <99715792+hromadianka@users.noreply.github.com>
2026-06-10 16:01:20 +0300

    Add Swagger docs

--- travel_app/settings.py
+++ travel_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'users',
     'projects',
     'places'
@@ -133,5 +134,6 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
-    )
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
--- travel_app/urls.py
+++ travel_app/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +27,11 @@ urlpatterns = [
     path('api/', include('projects.urls')),
     path('api/', include('users.urls')),
     path('api/', include('places.urls')),
+
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
